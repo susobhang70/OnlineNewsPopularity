9ce70330506db99aed48a75c95b3681a210e9e2a
Susobhan Ghosh <susobhang70@gmail.com>
2016-10-03 02:54:16 +0530

    Post Extractor added

---
+++ FetchPost/mashablepost.go
@@ -0,0 +1,52 @@
+package main
+import (
+	"github.com/sadlil/mashable"
+	"fmt"
+	"encoding/csv"
+	"os"
+	"io"
+)
+
+func main() {
+
+	f, err := os.Open("OnlineNewsPopularity.csv")
+	if err != nil {
+		panic("Cannot open file OnlineNewsPopularity.csv")
+	}
+	defer f.Close()
+
+	csvdata := csv.NewReader(f)
+
+	i := 0
+	for {
+		row, err := csvdata.Read()
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+				break
+			} else {
+				panic("Cannot read data")
+			}
+		}
+		if i == 0 {
+			i = i + 1
+			continue
+		}
+
+		c, err := mashable.New()
+		if err != nil {
+			panic("client creation failed")
+		}
+		// fmt.Printf(row[0] + "\n")
+		post, err := c.Posts().GetFromUrl(row[0])
+		if err != nil {
+			panic("Couldn't fetch url: " + row[0])
+		}
+		postcontent := post.Content
+		fmt.Printf(postcontent.Plain + "\n\n")
+		i = i + 1
+	}
+
+
+	
+}
